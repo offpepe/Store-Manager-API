fa8d001f535b62c0e5c64f55c813fdd198386a63
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-19 08:23:11 -0400

    feat: getAllProducts

--- models/productsModel.js
+++ models/productsModel.js
@@ -8,6 +8,13 @@ const createNewProduct = async (name, quantity) => {
     return result;  
 };
 
+const getAllProducts = async () => {
+    const db = await conn();
+    const result = await db.collection(coll).find().toArray();
+    return result;
+};
+
 module.exports = {
     createNewProduct,
+    getAllProducts,
 };
