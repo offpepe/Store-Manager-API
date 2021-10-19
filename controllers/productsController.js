6873ec890358c98fca29eb2b421b4119322d1513
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-19 08:27:34 -0400

    feat: getAll func

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,6 +6,12 @@ const createNewProduct = async (req, res) => {
     return res.status(200).json(result);
 };
 
+const getAllProducts = async (_req, res) => {
+    const products = await productsService.getAllProducts();
+    return res.status(200).json({ products });
+};
+
 module.exports = {
     createNewProduct,
+    getAllProducts,
 };
